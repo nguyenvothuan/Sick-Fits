1c37b54debdd43ca9fa66d75864fdbdcf586aec4
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-05-18 11:53:45 +0700

    add seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -27,6 +28,7 @@ const { withAuth } = createAuth({
   },
 });
 
+// wrap config inside withAuth to use authentication
 export default withAuth(
   config({
     // @ts-ignore
@@ -40,6 +42,11 @@ export default withAuth(
       adapter: 'mongoose',
       url: databaseURL,
       // TODO: Add data seeding here
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
