b13dec3cb713ca88a5e6685d83f7a59eb4b53e31
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-05-18 10:48:56 +0700

    2 way relation prod and prod img

--- backend/schemas/ProductImage.ts
+++ backend/schemas/ProductImage.ts
@@ -1,7 +1,7 @@
-import { cloudinaryImage } from '@keystone-next/cloudinary';
-import { text } from '@keystone-next/fields';
-import { list } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
+import { relationship, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+import { cloudinaryImage } from '@keystone-next/cloudinary';
 
 export const cloudinary = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
@@ -17,5 +17,11 @@ export const ProductImage = list({
       label: 'Source',
     }),
     altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product'],
+    },
   },
 });
