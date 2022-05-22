d2ed4ba899c51a3f7a77d291db92801ace5d7f8e
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-05-22 08:28:02 +0700

    query user

---
+++ frontend/components/User.js
@@ -0,0 +1,17 @@
+import { useQuery, gql } from '@apollo/client';
+
+export const CURRENT_USER_QUERY = gql`
+  query CURRENT_USER_QUERY {
+    authenticatedItem {
+      ... on User {
+        id
+        email
+      }
+    }
+  }
+`;
+
+export function useUser() {
+  const { data } = useQuery(CURRENT_USER_QUERY);
+  return data?.authenticatedItem;
+}
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,7 +1,7 @@
-export default function OrderPage() {
+export default function SignInPage() {
   return (
     <div>
       <p>Hello!</p>
     </div>
   );
-}g
+}
